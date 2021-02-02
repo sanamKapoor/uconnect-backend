75e45aff768c117a78ad971f807e35e88f61830f
Sanam Kapoor <47074142+sanamKapoor@users.noreply.github.com>
2021-02-02 11:17:27 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,7 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
     if(req.file){
+        console.log(req.file)
         fs.unlink(req.file.path, err => {
             console.log(err)
         })
