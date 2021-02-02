22987dd7a38f6f0ad92e5a2683e6f5afa55937d8
Sanam Kapoor <47074142+sanamKapoor@users.noreply.github.com>
2021-02-02 11:30:35 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,15 +50,15 @@ app.use((req, res, next) => {
     next(error);
 })
 
-app.use((error, req, res, next) => {
-    if(req.file){
-        console.log("file -", req.file)
-        fs.unlink(req.file.path, err => {
-            console.log(err)
-        })
-    }
-    if(res.headerSent){
-        return next(error)
-    }
-    res.status(error.code || 500).json({ message: error.message || 'Server Error!' })
-})
+// app.use((error, req, res, next) => {
+//     if(req.file){
+//         console.log("file -", req.file)
+//         fs.unlink(req.file.path, err => {
+//             console.log(err)
+//         })
+//     }
+//     if(res.headerSent){
+//         return next(error)
+//     }
+//     res.status(error.code || 500).json({ message: error.message || 'Server Error!' })
+// })
